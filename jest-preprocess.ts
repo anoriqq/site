a289ce201838d19db6d50d184b9bfce135942248
anoriqq <shota.yoshikawa@anoriqq.com>
2024-10-11 00:39:11 +0900

    feat: cleanup (#632)

--- jest-preprocess.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable */
-
-const babelOptions = {
-  presets: ['babel-preset-gatsby', '@babel/preset-typescript'],
-};
-
-module.exports = require('babel-jest').createTransformer(babelOptions);
