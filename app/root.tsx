96732fca7fbdcdf44707fd819eb9dfba25b454f8
anoriqq <shota.yoshikawa@anoriqq.com>
2024-10-13 14:52:13 +0900

    feat: configure entry css

--- app/root.tsx
+++ app/root.tsx
@@ -7,6 +7,8 @@ import {
 } from "@remix-run/react";
 import type { LinksFunction } from "@remix-run/node";
 
+import styles from "./root.css?url"
+
 export const links: LinksFunction = () => [
 	{ rel: "preconnect", href: "https://fonts.googleapis.com" },
 	{
@@ -18,6 +20,7 @@ export const links: LinksFunction = () => [
 		rel: "stylesheet",
 		href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
 	},
+	{ rel: "stylesheet", href: styles },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
