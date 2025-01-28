beee0ba63604c0725df4b5a08a7989998415f399
anoriqq <shota.yoshikawa@anoriqq.com>
2025-01-28 13:04:18 +0900

    feat: use html tags

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -12,12 +12,6 @@ export default function Index() {
 		height: "full",
 		"& > div": {
 			padding: "sm",
-			"& > a": {
-				color: "link",
-				_hover: {
-					textDecoration: "underline",
-				},
-			},
 		},
 	})
 	return (
@@ -25,21 +19,25 @@ export default function Index() {
 			<div>
 				<h1># anoriqq</h1>
 				<br/>
-				{"shota.yoshikawa[at]{THIS_DOMAIN}"}<br/>
-				Tokyo, Japan<br/>
+				<p>{"shota.yoshikawa[at]{THIS_DOMAIN}"}</p>
+				<p>Tokyo, Japan</p>
 				<br/>
 				<h2>## Link</h2>
 				<br/>
-				- <a href="https://github.com/anoriqq">GitHub</a>: あらゆるコード<br/>
-				- <a href="https://x.com/anoriqq">X</a>: プログラミング(仕事)とアニメのつぶやき<br/>
-				- <a href="https://mixi.social/@anoriqq">mixi2</a>: 起きてご飯食べて運動する<br/>
-				- <a href="https://anoriqq.hateblo.jp">はてなブログ</a>: 頭使って文章を書いたり日記を書いたり<br/>
+				<ul>
+					<li>- <a href="https://github.com/anoriqq">GitHub</a>: あらゆるコード</li>
+					<li>- <a href="https://x.com/anoriqq">X</a>: プログラミング(仕事)とアニメのつぶやき</li>
+					<li>- <a href="https://mixi.social/@anoriqq">mixi2</a>: 起きてご飯食べて運動する</li>
+					<li>- <a href="https://anoriqq.hateblo.jp">はてなブログ</a>: 頭使って文章を書いたり日記を書いたり</li>
+				</ul>
 				<br/>
 				<h2>## Work</h2>
 				<br/>
-				- 2022/01-2024/11: <a href="https://corp.cluster.mu/" target="_blank">クラスター株式会社</a><br/>
-				- 2020/10-2021/12: <a href="https://corp.cluster.mu/" target="_blank">クラスター株式会社</a> (インターン)<br/>
-				- 2018/01-2018/11: <a href="https://plaid.co.jp/" target="_blank">株式会社プレイド</a> (インターン)<br/>
+				<ul>
+					<li>- 2022/01-2024/11: <a href="https://corp.cluster.mu/" target="_blank">クラスター株式会社</a></li>
+					<li>- 2020/10-2021/12: <a href="https://corp.cluster.mu/" target="_blank">クラスター株式会社</a> (インターン)</li>
+					<li>- 2018/01-2018/11: <a href="https://plaid.co.jp/" target="_blank">株式会社プレイド</a> (インターン)</li>
+				</ul>
 			</div>
 		</div>
 	);
